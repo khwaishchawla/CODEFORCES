bfa30e454d1525b609c2de1dd4919b57b1fa21c4
khwaishchawla <126390524+khwaishchawla@users.noreply.github.com>
2024-10-23 03:26:46 +0530

    Create 703A.cpp

---
+++ 703A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cin >> n;
+    int m, c, ms = 0, cs = 0;
+    for(int i = 0; i < n; i++) 
+    {
+        cin >> m >> c;
+        if(m > c) 
+        {
+            ms++;
+        } 
+        else if(c > m) 
+        {
+            cs++;
+        }
+    }
+    
+    if(ms > cs) 
+    {
+        cout << "Mishka" << endl;
+    } 
+    else if(cs > ms) 
+    {
+        cout << "Chris" << endl;
+    } 
+    else 
+    {
+        cout << "Friendship is magic!^^" << endl;
+    }
+    
+    return 0;
+}
