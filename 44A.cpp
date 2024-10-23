e7728960f66f459134f93e531739d6a8dfda7b77
khwaishchawla <126390524+khwaishchawla@users.noreply.github.com>
2024-10-23 15:31:26 +0530

    Create 44A.cpp

---
+++ 44A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    set<pair<string, string>> uniqueLeaves;
+    for (int i = 0; i < n; ++i) {
+        string species, color;
+        cin >> species >> color;
+        uniqueLeaves.insert({species, color});
+    }
+
+    cout << uniqueLeaves.size() << endl;
+
+    return 0;
+}
