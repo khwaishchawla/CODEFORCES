d267ead53294cbc5a7896cf319128fe35de12d1e
khwaishchawla <126390524+khwaishchawla@users.noreply.github.com>
2024-10-23 15:37:26 +0530

    Create 92A.cpp

---
+++ 92A.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    int i = 1;
+    while (m >= i) {
+        m -= i;
+        i++;
+        if (i > n) {
+            i = 1; // Start again from the first walrus
+        }
+    }
+    
+    cout << m << endl;
+    return 0;
+}
