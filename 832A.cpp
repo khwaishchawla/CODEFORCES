9c009d20d388fd8f0a99133153a767ef692b96d7
Khwaish Chawla <126390524+khwaishchawla@users.noreply.github.com>
2024-10-24 09:02:01 +0530

    Create 832A.cpp

---
+++ 832A.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    long long n, k;
+    cin >> n >> k;
+
+    long long moves = n / k;
+
+    if (moves % 2 == 1) 
+    {
+        cout << "YES" << endl;
+    } 
+    else 
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
